efbc1ceba81e3244cea6cd702492993c35ea0cf0
Derek Fulginiti <derek@sharp.app>
2023-05-06 22:18:43 -0400

    Remove redundant comment
    I added this to the SDK class instead since that is what the developer will interact with

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,8 +1,6 @@
 export class Client {
     private readonly baseUrl = 'https://the-one-api.dev/v2';
 
-    // the /book endpoint does not require an api key.
-    // Please @see {@link https://the-one-api.dev/documentation} for more details.
     constructor(private apiKey?: string) {}
 
     public async fetch<T>(uri: string): Promise<T> {
