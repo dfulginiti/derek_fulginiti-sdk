c0f435ef1cb8fc33cf5bfa294cea3031f67ca0e1
Derek Fulginiti <derek@sharp.app>
2023-05-07 11:17:06 -0400

    Add client tests

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+import { Lotr } from './one_file_to_rule_them_all';
+
+export default Lotr;
---
+++ tests/client/client.test.ts
@@ -0,0 +1,50 @@
+import chai from 'chai';
+import chaiFetchMock from 'chai-fetch-mock';
+import fetchMock from 'fetch-mock';
+import { Client } from '../../src/client/client';
+
+chai.use(chaiFetchMock);
+
+let client: Client;
+
+const expect = chai.expect;
+
+const booksResponse = [
+    {
+        "_id": "5cf5805fb53e011a64671582",
+        "name": "The Fellowship Of The Ring"
+    },
+];
+
+describe('Client', () => {
+    before(() => client = new Client('key'));
+
+    it('returns json response on success', async () => {
+        fetchMock.get('/book', { 
+            'docs': booksResponse,
+         });
+
+        const books = await client.fetch('book');
+
+        expect(fetchMock).route('/book').to.have.been.called;
+
+        expect(books).to.be.an('object');
+        expect(books).to.have.key('docs');
+        expect(books.docs).to.be.eql(booksResponse);
+    });
+
+    it('throws error on failure', async () => {
+        fetchMock.get('/book', { 
+            'docs': booksResponse,
+            'config': { 'throws': new TypeError('Failed to fetch') }
+         });
+
+         try {
+             await client.fetch('book');
+         } catch (err) {
+            expect(err).to.eql(new TypeError('Failed to fetch'));
+         }
+    });
+
+    after(() => fetchMock.restore());
+});
